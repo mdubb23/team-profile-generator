39ff59e4cdfdc435a987e8c0d71df30c883c27a9
Matthew <willivms02@hotmail.com>
2022-05-15 18:27:05 -0400

    app layout

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const inquirer = require('inquirer')
+const fs = require('fs')
 
-console.log('Matthew Williams');
-console.log('1234@hotmail.com');
-console.log('1');
-console.log('1');
 
 const managerQuestions = () => {
     inquirer.prompt
@@ -29,4 +26,96 @@ const managerQuestions = () => {
             message: 'What is your managers office number?',
         }
     ])
-} 
+
+    addEmployee()
+    
+}
+
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'what type of employee would you like to add?',
+            choices: [
+                'Engineer',
+                'Intern',
+                'Done'
+            ]
+        }
+    ])
+
+    .then((data) => {
+        if(data.role === 'Engineer'){
+            engineerQuestions()
+        }
+        else if(data.role === 'Intern') {
+            internQuestions()
+        }
+        else{
+            createFile()
+        }
+    })
+}
+
+const internQuestions = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'employeeID',
+            message: 'What is your employee ID?'
+        },
+        {
+            type: 'input',
+            name: 'employeeEmail',
+            message: 'What is your Email?'
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What school do you attend?'
+        }
+    ])
+    
+}
+
+
+const engineerQuestions = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'employeeID',
+            message: 'What is your employee ID?'
+        },
+        {
+            type: 'input',
+            name: 'employeeEmail',
+            message: 'What is your Email?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your github username?'
+        }
+    ])
+}
+
+const createFile = function() {
+    return
+}
+
+const run = () => {
+    managerQuestions()
+
+}
+run()
